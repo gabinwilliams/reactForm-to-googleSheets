53ecaf33fe284566b262b734cac40df19d59ca8d
gabinwilliams <scoutsnknives@gmail.com>
2021-06-18 10:43:35 +0200

    updated jsx to have values

--- src/App.js
+++ src/App.js
@@ -24,28 +24,28 @@ nameHandler = (e) => {
 
   return (
     <Container fluid className="container">
-    <Header as='h2'>React Google Sheets!</Header>
-    <Form className="form">
-      <Form.Field>
-        <label>Name</label>
-        <input placeholder='Enter your name' />
-      </Form.Field>
-      <Form.Field>
-        <label>Age</label>
-        <input placeholder='Enter your age' />
-      </Form.Field>
-      <Form.Field>
-        <label>Salary</label>
-        <input placeholder='Enter your salary' />
-      </Form.Field>
-      <Form.Field>
-        <label>Hobby</label>
-        <input placeholder='Enter your hobby' />
-      </Form.Field>
-      
-      <Button color="blue" type='submit'>Submit</Button>
-    </Form>
-  </Container>
+        <Header as='h2'>React Google Sheets!</Header>
+        <Form className="form" onSubmit={this.submitHandler}>
+          <Form.Field>
+            <label>Name</label>
+            <input placeholder='Enter your name' type="text" name = "name" value = {name} onChange={this.changeHandler}/>
+          </Form.Field>
+          <Form.Field>
+            <label>Age</label>
+            <input placeholder='Enter your age' type="number" name = "age" value = {age} onChange={this.changeHandler}/>
+          </Form.Field>
+          <Form.Field>
+            <label>Salary</label>
+            <input placeholder='Enter your salary' type="number" name = "salary" value = {salary} onChange={this.changeHandler}/>
+          </Form.Field>
+          <Form.Field>
+            <label>Hobby</label>
+            <input placeholder='Enter your hobby' type="text" name = "hobby" value = {hobby} onChange={this.changeHandler}/>
+          </Form.Field>
+          
+          <Button color="blue" type='submit'>Submit</Button>
+        </Form>
+      </Container>
   );
 }
 
