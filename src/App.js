ad7ece38068540f8209888599c82da1cad19f12a
gabinwilliams <scoutsnknives@gmail.com>
2021-06-18 10:55:52 +0200

    adding changeHandler function

--- src/App.js
+++ src/App.js
@@ -9,18 +9,26 @@ const [age, setAge] = useState('');
 const [salary, setSalary] =useState('');
 const [hobby, setHobby] =useState('');
 
-nameHandler = (e) => {
+changeHandler = (e) => {
+  switch (e.target.name) {
+    case 'name':
+      setName(e.target.value)
+      break;
+    case 'age':
+      setAge(e.target.value)
+      break;
+    case 'salary':
+      setSalary(e.target.value)
+      
+      break;
+      case 'hobby':
+        break;
+    default:
+      console.log(`Sorry, we are out of ${expr}.`);
+  }
   setName(e.target.value);
 }
-nameHandler = (e) => {
-  setAge(e.target.value);
-}
-nameHandler = (e) => {
-  setSalary(e.target.value);
-}
-nameHandler = (e) => {
-  setHobby(e.target.value);
-}
+
 
   return (
     <Container fluid className="container">
@@ -28,19 +36,19 @@ nameHandler = (e) => {
         <Form className="form" onSubmit={this.submitHandler}>
           <Form.Field>
             <label>Name</label>
-            <input placeholder='Enter your name' type="text" name = "name" value = {name} onChange={this.changeHandler}/>
+            <input placeholder='Enter your name' type="text" name = "name" value = {name} onChange={changeHandler}/>
           </Form.Field>
           <Form.Field>
             <label>Age</label>
-            <input placeholder='Enter your age' type="number" name = "age" value = {age} onChange={this.changeHandler}/>
+            <input placeholder='Enter your age' type="number" name = "age" value = {age} onChange={changeHandler}/>
           </Form.Field>
           <Form.Field>
             <label>Salary</label>
-            <input placeholder='Enter your salary' type="number" name = "salary" value = {salary} onChange={this.changeHandler}/>
+            <input placeholder='Enter your salary' type="number" name = "salary" value = {salary} onChange={changeHandler}/>
           </Form.Field>
           <Form.Field>
             <label>Hobby</label>
-            <input placeholder='Enter your hobby' type="text" name = "hobby" value = {hobby} onChange={this.changeHandler}/>
+            <input placeholder='Enter your hobby' type="text" name = "hobby" value = {hobby} onChange={changeHandler}/>
           </Form.Field>
           
           <Button color="blue" type='submit'>Submit</Button>
